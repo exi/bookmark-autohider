39f6798f6958049d63c30ba3ec2f80c52a60d321
Reno Reckling <e-github@wthack.de>
2011-03-27 22:48:52 +0200

    core cleanup
    i removed all the call-by-name intervals and timeouts
    removed ff-overlay.js as it was obsoloete
    switch to real private variables, exposing no public values

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -1,164 +1,205 @@
-var bookmarkhider = {
-    prefix: "extensions.bookmarkhider.", //the memory path prefix
-    prefs: Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch), //memory service
-    out: 1, //wheter the mouse is outside of the toolbar
-    lastover: 0, //the last time in ms when the mouse was above the toolbar
-    hideInterval: 50, //the tick intervall in ms used to slide the toolbar in
-    intervalObj: undefined, //the interval object used to animate
-    curHeight: 0, //the current height, first values is calculated by this.show()
-    origHeight: 0, //the origial height, first values is calculated by this.show()
-    overTimer: undefined,
-    outTimer: undefined,
-    onLoad: function() {
+var bookmarkhider = function() {
+    var me = this;
+
+    me.prefix = "extensions.bookmarkhider."; //the memory path prefix
+    me.prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch); //memory service
+    me.out = true; //wheter the mouse is outside of the toolbar
+    me.lastover = 0; //the last time in ms when the mouse was above the toolbar
+    me.hideInterval = 50; //the tick interval in ms used to slide the toolbar in
+    me.intervalObj = undefined; //the interval object used to animate
+    me.curHeight = 0; //the current height, first values is calculated by this.show()
+    me.origHeight = 0; //the origial height, first values is calculated by this.show()
+    me.overTimer = undefined;
+    me.outTimer = undefined;
+
+    me.onLoad = function() {
         // initialization code
-        this.initialized = true;
+        me.initialized = true;
+        me.getToolbar().addEventListener("mouseover", function() { return me.mouseover;}(), false);
+        me.getToolbar().addEventListener("dragenter", function() { return me.mouseover;}(), false);
+        me.getUrlbarContainer().addEventListener("mouseover", function() { return me.mouseover;}(), false);
+        me.getUrlbarContainer().addEventListener("dragenter", function() { return me.mouseover;}(), false);
+        me.getContent().addEventListener("mouseover", function() { return me.mouseout;}(), false);
     },
-    getSlideBool: function() {
-        return this.prefs.getBoolPref(this.prefix+"slide");
-    },
-    getSlidetime: function() {
-        return this.prefs.getIntPref(this.prefix+"slidetime");
-    },
-    getOpentime: function() {
-        return this.prefs.getIntPref(this.prefix+"opentime");
-    },
-    getOpendelay: function() {
-        return this.prefs.getIntPref(this.prefix+"opendelay");
-    },
-    getStyle: function() {
-        return this.getToolbar().style;
-    },
-    getCompHeight: function() {
-        var docObj = this.getToolbar();
+
+    me.getToolbar = function() {
+        return document.getElementById("PersonalToolbar");
+    };
+
+    me.getUrlbarContainer = function() {
+        return document.getElementById("urlbar-container");
+    };
+
+    me.getContent = function() {
+        return document.getElementById("content");
+    };
+
+    me.getSlideBool = function() {
+        return me.prefs.getBoolPref(me.prefix+"slide");
+    };
+
+    me.getSlidetime = function() {
+        return me.prefs.getIntPref(me.prefix+"slidetime");
+    };
+
+    me.getOpentime = function() {
+        return me.prefs.getIntPref(me.prefix+"opentime");
+    };
+
+    me.getOpendelay = function() {
+        return me.prefs.getIntPref(me.prefix+"opendelay");
+    };
+
+    me.getStyle = function() {
+        return me.getToolbar().style;
+    };
+
+    me.getCompHeight = function() {
+        var docObj = me.getToolbar();
 	var height = parseFloat(document.defaultView.getComputedStyle(docObj, "").getPropertyValue("height"));
         return height;  
-    },
-    getToolbar: function() {
-        return document.getElementById("PersonalToolbar");
-    },
+    };
+
     //check for macos
-    isMacOS: function() {
+    me.isMacOS = function() {
         return navigator.appVersion.toLowerCase().indexOf("mac") > 0;;
-    },
-    useMultiBmToolbar: function() {
+    };
+
+    me.useMultiBmToolbar = function() {
 	var ret = false;
 	try {
-	    ret = this.prefs.getBoolPref("extensions.multibmtoolbar.enable");
+	    ret = me.prefs.getBoolPref("extensions.multibmtoolbar.enable");
 	} catch(e) { }
 	return ret;
-    },
-    //this function is called whenever the mouse leaves the bookmark toolbar
-    mouseout: function() {
-        if ( !this.out ) {
-	    if ( this.outTimer)
-	        window.clearTimeout( this.outTimer );
-            this.out = true;
-            var mytime = ( new Date() ).getTime();
-            if (mytime - this.lastover < this.getOpentime() ) {
-                this.outTimer = window.setTimeout("bookmarkhider.hide();", this.getOpentime() - (mytime - this.lastover));
-            } else {
-                this.hide();
-            }
-        }
-    },
-    //this function is calles whenever the mouse enters the toolbar
-    mouseover: function() {
-        this.lastover = ( new Date() ).getTime();
-        if ( this.out ) {
-	    if ( this.overTimer )
-	        window.clearTimeout( this.overtimer );
-            this.clearInterval();
-            this.out = false;
-            if (this.getOpendelay() > 0) {
-                this.overTimer = window.setTimeout('bookmarkhider.show();',this.getOpendelay());
-            } else {
-                this.show();
-            }
-        }
-    },
+    };
+
     //let the toobar stay open
-    stayopen: function() {
-        this.out = false;
-    },
+    me.stayopen = function() {
+        me.out = false;
+    };
+
     //this function resets the toolbar stylings (aka, opens the toolbar) 
-    resetStyle: function() {
-        var style = this.getStyle();
+    me.resetStyle = function() {
+        var style = me.getStyle();
         style.minHeight = "5px";
         style.maxHeight = "";
 	style.height = "";
         style.overflow = "";
 	//fixing the multibar issues
-	if ( this.useMultiBmToolbar() ) {
+	if ( me.useMultiBmToolbar() ) {
 	    style.setProperty("overflow-y","visible",null);
 	}
 
-    },
+    };
+
     //this function sets the interval object
-    setInterval: function (func) {
-        if ( typeof(this.intervalObj) == 'undefined' )
-            this.intervalObj = window.setInterval(func, this.hideInterval );
-    },
-    //this function cleans the interval object
-    clearInterval: function() {
-        window.clearInterval(this.intervalObj);
-        this.intervalObj = undefined;
-    },
-    //this function collapses the bar
-    collapse: function() {
-        this.getToolbar().collapsed=true;
-        this.getStyle().visibility="collapse";
-    },
-    //this function is called whenever the toolbar should show up
-    show: function() {
-        if ( !this.out ) {
-            this.resetStyle();
-            this.getToolbar().collapsed = false;
-            this.getStyle().visibility = "visible";
-	    this.curHeight = this.getCompHeight();
-	    this.origHeight = this.getCompHeight();
+    me.setMyInterval = function (cb) {
+        if ( "undefined" == typeof(me.intervalObj) ) {
+            me.intervalObj = window.setInterval(cb, me.hideInterval );
         }
-    },
+    };
+
+    //this function cleans the interval object
+    me.clearMyInterval = function() {
+        if("undefined" !== typeof(me.intervalObj)) {
+            window.clearInterval(me.intervalObj);
+            me.intervalObj = undefined;
+        }
+    };
+
+    //this function collapses the bar
+    me.collapse = function() {
+        me.getToolbar().collapsed=true;
+        me.getStyle().visibility="collapse";
+    };
+
+    //this function is called whenever the toolbar should show up
+    me.show = function() {
+        if ( !me.out ) {
+            me.resetStyle();
+            me.getToolbar().collapsed = false;
+            me.getStyle().visibility = "visible";
+	    me.curHeight = me.getCompHeight();
+	    me.origHeight = me.getCompHeight();
+        }
+    };
+
     //this function is called whenever the toolbar should hide and performs the movement
-    hide: function() {
-        if ( this.out ) {
-            var style = this.getStyle();
-            if (this.curHeight > 1 && this.getSlideBool() ) {
-                var stepping = (this.origHeight/this.getSlidetime())*this.hideInterval;
+    me.hide = function() {
+        if ( me.out ) {
+            var style = me.getStyle();
+            if (me.curHeight > 1 && me.getSlideBool() ) {
+                var stepping = (me.origHeight/me.getSlidetime())*me.hideInterval;
 
                 if (isNaN(parseFloat(style.minHeight))) {
-                    this.resetStyle();
+                    me.resetStyle();
                 }
 
-                if (this.isMacOS()) {
-                    this.curHeight = style.minHeight;//macos dirty hack
+                if (me.isMacOS()) {
+                    me.curHeight = style.minHeight;//macos dirty hack
                 }
 
-                var newH = (this.curHeight-stepping)+"px";
+                var newH = (me.curHeight-stepping)+"px";
 
                 style.minHeight = newH;
                 style.maxHeight = newH;
                 style.height = newH;
                 style.overflow = "hidden";
 
-		if ( this.useMultiBmToolbar() ) {
+		if ( me.useMultiBmToolbar() ) {
 		    style.setProperty("overflow-y","hidden",null);
 		}
 
-                this.setInterval("bookmarkhider.hide();");
-		this.curHeight = parseFloat(newH);
-		if ( this.curHeight < 1 ) {
-		    this.curHeight = 0.1;
+                me.setMyInterval(function() { return me.hide;}());
+		me.curHeight = parseFloat(newH);
+		if ( me.curHeight < 1 ) {
+		    me.curHeight = 0.1;
 		}
             } else {
-                this.clearInterval();
+                me.clearMyInterval();
                 style.minHeight = "0px";
                 style.maxHeight = "0px";
-                this.out = true;
-                this.lastover = 0;
-                this.collapse();
+                me.out = true;
+                me.lastover = 0;
+                me.collapse();
             }
         }
     }
-};
 
-window.addEventListener("load", bookmarkhider.onLoad, false);
+    //this function is called whenever the mouse leaves the bookmark toolbar
+    me.mouseout = function() {
+        if ( !me.out ) {
+	    if ( me.outTimer)
+	        window.clearTimeout( me.outTimer );
+            me.out = true;
+            var mytime = ( new Date() ).getTime();
+            if (mytime - me.lastover < me.getOpentime() ) {
+                me.outTimer = window.setTimeout(function() { return me.hide;}(), me.getOpentime() - (mytime - me.lastover));
+            } else {
+                me.hide();
+            }
+        }
+    };
+
+    //this function is calles whenever the mouse enters the toolbar
+    me.mouseover = function() {
+        me.lastover = ( new Date() ).getTime();
+        if ( me.out ) {
+	    if ( "undefined" !== typeof me.overTimer )
+	        window.clearTimeout( me.overTimer );
+            me.clearInterval();
+            me.out = false;
+            if (me.getOpendelay() > 0) {
+                me.overTimer = window.setTimeout(function() { return me.show;}(),me.getOpendelay());
+            } else {
+                //me.show();
+            }
+        }
+    };
+
+    window.addEventListener("load", function () { return me.onLoad; }(), false);
+
+    //no public interface
+    return {};
+
+}();
